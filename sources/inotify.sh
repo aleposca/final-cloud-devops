6d4fe372d2e7ecc64d5ecd53e3bd5ae395d2b74c
Ale <ale.posca@gmail.com>
2018-06-27 11:22:27 -0300

    Final

--- sources/inotify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="/watched"
-inotifywait -m -e create "$DIR" | while read f
-
-do
-    # you may want to release the monkey after the test :)
-    echo monkey
-    # <whatever_command_or_script_you_liketorun>
-done
